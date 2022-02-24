845e0135821d72224e22c2ad44f626c9552255c1
Noah Perry <52791015+Tehcn@users.noreply.github.com>
2022-02-24 07:55:35 -0600

    gamer

--- main.cpp
+++ main.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
 
 #include "color.h"
+#include "ray.h"
 #include "vec3.h"
+
+color ray_color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
 
 int main() {
     // Image
-    const int image_width = 256;
-    const int image_height = 256;
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // Camera
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
 
     // Render
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
