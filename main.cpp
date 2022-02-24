e0fd38ae59b0272e16e4150550490ef8df89a45c
Noah Perry <52791015+Tehcn@users.noreply.github.com>
2022-02-24 12:27:28 -0600

    did stuff

--- main.cpp
+++ main.cpp
@@ -24,6 +24,7 @@ int main() {
     auto origin = point3(0, 0, 0);
     auto horizontal = vec3(viewport_width, 0, 0);
     auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
 
     // Render
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
@@ -31,9 +32,10 @@ int main() {
     for(int j = image_height-1; j >= 0; --j) {
         std::cerr << "\rScanlines remaning: " << j << ' ' << std::flush;
         for(int i = 0; i < image_width; ++i) {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.25;
+            auto u = double(i) / (image_width - 1);
+            auto v = double(j) / (image_height - 1);
+            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            write_color(std::cout, pixel_color);
         }
     }
 }
